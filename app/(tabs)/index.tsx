ca5531e5ac40b579f50fb89519b57a5c005d54a0
Ferperemero86 <fernandoperezmerono@gmail.com>
2025-03-28 17:31:08 +0000

    Create Search bar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView, ScrollViewComponent } from "react-native";
+
 import { Link } from 'expo-router';
+import { useRouter } from "expo-router";
+
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-5xl">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image 
+        source={images.bg} 
+        className="absolute w-full z-0" />
+
+      <ScrollView 
+        className="flex-1 px-5" 
+        showsVerticalScrollIndicator={false} 
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}>
+
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 px-5">
+          <SearchBar placeholder="Search movie" onPress={() => router.push("/search")} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
